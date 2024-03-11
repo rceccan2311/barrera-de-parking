a2051022ac3cb0c382db534a4bf3f5a5d5d733d4
rceccan2311 <132548157+rceccan2311@users.noreply.github.com>
2024-03-11 10:10:24 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P0, 180)
+    basic.showString("Abierto")
 })
 input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P0, 90)
+    basic.showString("cerrado")
 })
 pins.servoWritePin(AnalogPin.P0, 90)
